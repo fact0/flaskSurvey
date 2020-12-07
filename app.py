7dba045c233c64586367b1070b001511f2216b17
Michael Schienbein <moomba0@gmail.com>
2020-12-07 15:46:34 +0300

    added session

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from random import randint, choice, sample
 from surveys import satisfaction_survey as survey
 app = Flask(__name__)
 
@@ -8,8 +7,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
-
-responses = []
 
 
 @app.route('/')
@@ -19,9 +16,19 @@ def index():
     return render_template("index.html", title=title, instructions=instructions)
 
 
+@app.route("/begin", methods=["GET", "POST"])
+def start_survey():
+    """Clear the session of responses."""
+
+    session['responses'] = []
+
+    return redirect("/questions/0")
+
+
 @app.route('/questions/<int:qid>')
 def get_questions(qid):
     title = survey.title
+    responses = session.get('responses')
 
     if (responses is None):
         # trying to access question page too soon
@@ -42,7 +49,9 @@ def get_questions(qid):
 @app.route('/answer', methods=["POST"])
 def handle_answer():
     choice = request.form['answer']
+    responses = session['responses']
     responses.append(choice)
+    session['responses'] = responses
 
     if (len(responses) == len(survey.questions)):
         return redirect("/complete")
